c78f66029633f21724ec2e023f580d6c81922be9
lorenagrassilo <lorena.rodrigues@ucsal.edu.br>
2024-06-10 20:49:11 -0300

    feat: enhance payment system menu with a loop and exit option
    Added a loop to keep displaying the payment options menu until the user chooses to exit.
    Updated menu to include an option to exit the application.
    Improved user interaction by handling invalid options and prompting for input more clearly.

--- ComponenteDePagamento/src/main/java/domain/SistemaPagamento.java
+++ ComponenteDePagamento/src/main/java/domain/SistemaPagamento.java
@@ -19,32 +19,47 @@ public class SistemaPagamento {
      */
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         Notificacao notificacao = new NotificacaoConsole();
         Pagamento pagamento = null;
+        int opcao = -1;
+        
+        while (opcao != 9) {
+            String menu = """
+                    Sistema de Pagamento
 
-        System.out.println("Escolha a forma de pagamento:");
-        System.out.println("1. Boleto");
-        System.out.println("2. Cartão");
-        System.out.println("3. PIX");
+                    Escolha a forma de pagamento:
+                    1 - Boleto
+                    2 - Cartão
+                    3 - PIX
+                    4 - Sair ...""";
 
-        int opcao = scanner.nextInt();
+            System.out.println(menu);
 
-        switch (opcao) {
-            case 1:
-                pagamento = new PagamentoBoleto();
-                break;
-            case 2:
-                System.out.println("Digite o número de parcelas:");
-                int parcelas = scanner.nextInt();
-                pagamento = new PagamentoCartao(parcelas);
-                break;
-            case 3:
-                pagamento = new PagamentoPIX();
-                break;
-            default:
-                System.out.println("Opção inválida!");
-                return;
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    pagamento = new PagamentoBoleto();
+                    break;
+                case 2:
+                    System.out.println("Digite o número de parcelas:");
+                    int parcelas = scanner.nextInt();
+                    pagamento = new PagamentoCartao(parcelas);
+                    break;
+                case 3:
+                    pagamento = new PagamentoPIX();
+                    break;
+                case 4:
+                    System.out.println("Closing the application!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    return;
+            }
         }
 
         System.out.println("Digite o valor do pagamento:");
